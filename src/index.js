5b8099eb76630d06a4294a791b0b39047ca95d03
deejayeaster <deejayeaster@live.com>
2019-08-14 16:03:52 -0400

    added redux depends

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { carReducer } from "./reducers";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
-export const store = createStore(titleReducer);
+export const store = createStore(carReducer);
 
 // console.log("store: ", store);
 
